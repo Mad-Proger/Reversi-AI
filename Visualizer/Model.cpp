75d91ebb2494679fb3e2081033c67263ed4988a1
Mikhail Zaytsev <69989867+Mad-Proger@users.noreply.github.com>
2022-01-25 01:44:30 +0300

    Refactoring (#1)
    * fix Rect initializations
    
    * replace tabs with spaces
    
    * fix index types
    
    * fix trailing newlines
    
    * change Desk index types
    
    * change exception handling in model evaluation
    
    * AIPlayer refactoring
    
    * Desk refactoring
    
    * Game refactoring
    
    * GameWindow refactoring

--- Visualizer/Model.cpp
+++ Visualizer/Model.cpp
@@ -4,71 +4,70 @@ Model::Model() : inputNeurons(), n(), weights() {
 }
 
 Model::Model(const Model& model) : inputNeurons(model.inputNeurons), n(model.n) {
-	weights = new Matrix[n];
-	for (size_t i = 0; i < n; ++i) {
-		weights[i] = model.weights[i];
-	}
+    weights = new Matrix[n];
+    for (size_t i = 0; i < n; ++i) {
+        weights[i] = model.weights[i];
+    }
 }
 
 Model::~Model() {
-	delete[] weights;
+    delete[] weights;
 }
 
 Model Model::operator=(const Model& model) {
-	if (n != model.n) {
-		delete[] weights;
-		weights = new Matrix[model.n];
-	}
-	
-	inputNeurons = model.inputNeurons;
-	n = model.n;
+    if (n != model.n) {
+        delete[] weights;
+        weights = new Matrix[model.n];
+    }
 
-	for (size_t i = 0; i < n; ++i) {
-		weights[i] = model.weights[i];
-	}
+    inputNeurons = model.inputNeurons;
+    n = model.n;
 
-	return *this;
+    for (size_t i = 0; i < n; ++i) {
+        weights[i] = model.weights[i];
+    }
+
+    return *this;
 }
 
 void Model::load(const std::string& filename) {
-	std::ifstream modelData(filename);
-	if (!modelData.is_open()) {
-		throw std::invalid_argument("Couldn't open model file");
-	}
+    std::ifstream modelData(filename);
+    if (!modelData.is_open()) {
+        throw std::invalid_argument("Couldn't open model file");
+    }
 
-	this->~Model();
+    this->~Model();
 
-	modelData >> inputNeurons >> n;
-	std::vector<size_t> hiddenNeurons(n);
-	for (size_t i = 0; i < n; ++i) {
-		modelData >> hiddenNeurons[i];
-	}
+    modelData >> inputNeurons >> n;
+    std::vector<size_t> hiddenNeurons(n);
+    for (size_t i = 0; i < n; ++i) {
+        modelData >> hiddenNeurons[i];
+    }
 
-	weights = new Matrix[n];
-	size_t prevNeurons = inputNeurons;
-	for (size_t i = 0; i < n; ++i) {
-		weights[i] = Matrix(hiddenNeurons[i], prevNeurons);
-		
-		for (size_t x = 0; x < hiddenNeurons[i]; ++x) {
-			for (size_t y = 0; y < prevNeurons; ++y) {
-				modelData >> weights[i](x, y);
-			}
-		}
+    weights = new Matrix[n];
+    size_t prevNeurons = inputNeurons;
+    for (size_t i = 0; i < n; ++i) {
+        weights[i] = Matrix(hiddenNeurons[i], prevNeurons);
 
-		prevNeurons = hiddenNeurons[i];
-	}
+        for (size_t x = 0; x < hiddenNeurons[i]; ++x) {
+            for (size_t y = 0; y < prevNeurons; ++y) {
+                modelData >> weights[i](x, y);
+            }
+        }
+
+        prevNeurons = hiddenNeurons[i];
+    }
 }
 
 Matrix Model::operator()(Matrix data) const {
-	if (data.getSize().first != inputNeurons || 
-		data.getSize().second != 1) {
-		throw std::invalid_argument("Invalid input shape");
-	}
+    try {
+        for (size_t i = 0; i < n; ++i) {
+            data = weights[i] * data;
+            data.apply(std::tanh);
+        }
+    } catch (std::invalid_argument) {
+        throw std::invalid_argument("Invalid input shape");
+    }
 
-	for (size_t i = 0; i < n; ++i) {
-		data = weights[i] * data;
-		data.apply(std::tanhf);
-	}
-
-	return data;
+    return data;
 }
